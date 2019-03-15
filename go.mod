4d883dccbe0b38a751788936f49c02c973718ef9
Vadim Ashikhman <v.ashikhman@gmail.com>
2019-03-16 02:37:02 +0300

    Working on parallel executing of scenarios

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.12
 
 require (
 	github.com/cucumber/gherkin-go v5.1.0+incompatible
+	github.com/lib/pq v1.0.0
 	github.com/pkg/errors v0.8.1
 	gopkg.in/yaml.v2 v2.2.2
 )
--- pkg/plugin/http/http.go
+++ pkg/plugin/http/http.go
@@ -1,11 +1,26 @@
 package http
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/lib/pq"
 	"github.com/proofhouse/proofhouse/pkg/plugin"
 )
 
 func init() {
+	connStr := "host=127.0.0.1 port=13100 user=postgres password=postgres dbname=test sslmode=disable"
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		panic(err)
+	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	_ = db
+
 	p := plugin.New("http")
 	p.AddStep("I send :num requests to :url", func(p plugin.Params) {
 		url := p.String("url")
